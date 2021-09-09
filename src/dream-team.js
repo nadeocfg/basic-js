3cae3002b7b2a706312e80efa2aa6db3a45b21ab
a.khegay <khegay.alexey@mail.ru>
2021-09-09 21:14:12 +0600

    feat: add few solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,28 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(arr) {
+  if (!arr) {
+    return false;
+  }
+
+  let res = '';
+  for (let i = 0; i < arr.length; i += 1) {
+    if (!!arr[i] && !!arr[i][0] && typeof arr[i] === 'string') {
+      res += arr[i].trim().toUpperCase()[0];
+    }
+  }
+
+  return res === '' ? false : res.split('').sort().join('');
 }
--- src/what-season.js
+++ src/what-season.js
@@ -2,16 +2,42 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (isNaN(Date.parse(date))) {
+    throw 'Invalid date!';
+  }
+
+  const seasons = {
+    0: 'winter',
+    1: 'winter',
+    2: 'spring',
+    3: 'spring',
+    4: 'spring',
+    5: 'summer',
+    6: 'summer',
+    7: 'summer',
+    8: 'autumn',
+    9: 'autumn',
+    10: 'autumn',
+    11: 'winter',
+  };
+
+  return seasons[date.getMonth()];
 }
+
+// console.log(getSeason('foo'));
+// console.log(getSeason({ John: 'Smith' }));
+// console.log(getSeason(20192701));
