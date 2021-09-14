aeb60cb0fe66d9ca71b55c76052c83dc34c8d816
a.khegay <khegay.alexey@mail.ru>
2021-09-14 10:38:16 +0600

    feat: add solution for carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,34 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(str) {
-  if (typeof str !== 'string' || !str || Number(str) !== NaN) {
-    return false;
+export default function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity === 'string' &&
+    sampleActivity &&
+    parseFloat(sampleActivity) &&
+    parseFloat(sampleActivity) > 0 &&
+    parseFloat(sampleActivity) < MODERN_ACTIVITY
+  ) {
+    const k = 0.693 / HALF_LIFE_PERIOD;
+
+    const t = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / k);
+
+    return t;
   }
+
+  return false;
 }
+
+// console.log(dateSample('ACTIVITY OVER 9000'));
+// console.log(dateSample('one'));
+// console.log(dateSample(''));
+// console.log(dateSample(' '));
+// console.log(dateSample(' \n\t\r'));
+
+// console.log(dateSample('3'), 13305);
+// console.log(dateSample('1'), 22387);
+// console.log(dateSample('9'), 4223);
+// console.log(dateSample('11'), 2564);
+
+// const k = 0.693 / HALF_LIFE_PERIOD
+// Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / k)
--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  const res = [];
+  const arr = str.split('');
+
+  let tempChar = '';
+  let counter = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (!tempChar || tempChar !== arr[i]) {
+      res.push((counter > 1 ? counter : '') + tempChar);
+      tempChar = arr[i];
+      counter = 1;
+    } else {
+      counter += 1;
+    }
+
+    if (i === arr.length - 1) {
+      res.push((counter > 1 ? counter : '') + tempChar);
+    }
+  }
+
+  return res.join('');
 }
+
+// console.log(encodeLine('abbcca'));
+// console.log('a2b2ca');
+
+// console.log(encodeLine('aabbccc'));
+// console.log('2a2b3c');
